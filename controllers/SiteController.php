7bd07b9f1ecd4deabc9d9ec6175ca66b60fda704
Evgeniy Tkachenko <et.coder@gmail.com>
2014-04-07 21:53:17 +0300

    refactor

---
+++ controllers/SiteController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\PostForm;
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+use yii\web\HttpException;
+
+class SiteController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout'],
+                'rules' => [
+                    [
+                        'actions' => ['logout'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    public function actionView($id)
+    {
+        $post = new PostForm();
+
+        if (isset($post->sectors[$id])) {
+            $post->sector = $id;
+            $post->content = file_get_contents($post->getPathContent());
+        } else {
+            throw new HttpException(404);
+        }
+
+        return $this->render('view', ['post' => $post]);
+    }
+
+    public function actionLogin()
+    {
+        if (!\Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        } else {
+            return $this->render('login', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        } else {
+            return $this->render('contact', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
---
+++ views/site/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @var yii\web\View $this
+ */
+use yii\helpers\Url;
+
+$this->title = 'My Yii Application';
+$post = new \app\models\PostForm();
+?>
+<div class="grid row height100">
+
+    <section class="col-lg-4 bg1 grid1">
+        <a href="<?= Url::toRoute(['view', 'id' => 1]) ?>">
+            <h1><?= $post->sectors[1] ?></h1>
+        </a>
+    </section>
+
+    <section class="col-lg-4 bg2 grid2"><h1><?= $post->sectors[2] ?></h1></section>
+
+    <section class="col-lg-4 bg3 grid3"><h1><?= $post->sectors[3] ?></h1></section>
+
+    <section class="col-lg-4 bg4 grid4"><h1><?= $post->sectors[4] ?></h1></section>
+
+    <section class="col-lg-4 bg5 grid5">
+        <small>Частное унитарное предприятие по оказанию услуг</small>
+        <h1>Студия "СПЭС"</h1>
+        <hr>
+        <img
+            src="http://image.spreadshirt.net/image-server/v1/designs/14787655,width%3D178,height%3D178/60-year-diamond-jubilee-queen-elizabeth-logo.png">
+        <footer>
+            <p>2014 - spas</p>
+
+            <p data-app-version></p>
+        </footer>
+    </section>
+
+    <section class="col-lg-4 bg6 grid6">
+        <a href="<?= Url::toRoute(['view', 'id' => 5]) ?>">
+            <h1><?= $post->sectors[5] ?></h1>
+        </a>
+    </section>
+
+</div>
+
